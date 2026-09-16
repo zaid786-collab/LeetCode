cb742360799a518d9eb00e74b4b4d4802fbc9578
Mohammad Zaid Khan <zaidkhan24082006@gmail.com>
2026-09-16 23:19:23 +0530

    Time: 4 ms (22.37%), Space: 13.4 MB (34.24%) - LeetHub

--- 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int appendCharacters(string s, string t) {
-        
+        int j = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (j < t.length() && s[i] == t[j]) {
+                j++;
+            }
+        }
+
+        return t.length() - j;
     }
 };
