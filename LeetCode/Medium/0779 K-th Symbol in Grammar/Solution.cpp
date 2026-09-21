a1e8c3fa869a00c49aee9c6a40ab31a1b79c4935
Mohammad Zaid Khan <zaidkhan24082006@gmail.com>
2026-09-21 15:17:04 +0530

    Add solution for LeetCode - K-th Symbol in Grammar

--- LeetCode/Medium/0779 K-th Symbol in Grammar/Solution.cpp
+++ LeetCode/Medium/0779 K-th Symbol in Grammar/Solution.cpp
@@ -1,30 +1,10 @@
 class Solution {
 public:
     int kthGrammar(int n, int k) {
-        vector<int> prev(1,0);
-        vector<int> main;
-
-        int i = n;
-        while(i--) {
-
-            vector<int> next;
-
-            int x = prev.size();
-            for(int i=0;i<x;i++) {
-                if(prev[i] == 0) {
-                    next.push_back(0);
-                    next.push_back(1);
-                }
-                else if(prev[i] == 1) {
-                    next.push_back(1);
-                    next.push_back(0);
-                }
-            }
-            prev = next;
+        if(n == 1) {
+            return 0;
         }
 
-        main = prev;
-
-        return main[k-1];
+        int x = (2^(n-1));
     }
 };
